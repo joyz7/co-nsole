8e603a5511a2b518824fef39cbabf76f46d48916
Joy Zheng <58274342+joyz7@users.noreply.github.com>
2022-11-19 16:26:29 -0500

    Initial landing page

---
+++ App.js
@@ -0,0 +1,9 @@
+let situation
+
+const role = document.querySelector('.role');
+role.addEventListener('click', event => {
+    const node = document.createElement("div");
+    //node.setAttribute();
+    node.innerHTML = `
+    `;
+});
