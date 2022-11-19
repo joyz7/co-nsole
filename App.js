80ee91ded815b46b48c06293c1f081c3d1e4e05b
elyknehc <kchen727@uwo.ca>
2022-11-19 18:55:22 -0500

    update website

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-let situation
+let situation;
+button = document.getElementById("test");
 
-const role = document.querySelector('.role');
-role.addEventListener('click', event => {
-    const node = document.createElement("div");
-    //node.setAttribute();
-    node.innerHTML = `
+const role = document.querySelector(".role");
+role.addEventListener("click", (event) => {
+	const node = document.createElement("div");
+	//node.setAttribute();
+	node.innerHTML = `
     `;
 });
